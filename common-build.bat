6eb24436f0289c021f40ba7dd2882907fcd43714
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-04 10:27:11 +0900

    ビルド用バッチファイルを更新

--- common-build.bat
+++ common-build.bat
@@ -67,7 +67,13 @@ popd
 @REM   "CasProcessor  のビルド"
 
 pushd  CasProcessor
-%build_cmd%  -p:Platform=Win32  -p:Configuration=Release  ^
+%build_cmd%  -p:Platform=Win32 -p:Configuration=Debug       ^
+    %retarget_solution%  CasProcessor.sln
+%build_cmd%  -p:Platform=Win32 -p:Configuration=Release     ^
+    %retarget_solution%  CasProcessor.sln
+%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       ^
+    %retarget_solution%  CasProcessor.sln
+%build_cmd%  -p:Platform=x86   -p:Configuration=Release     ^
     %retarget_solution%  CasProcessor.sln
 popd
 
