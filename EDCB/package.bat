6097b7ab6faf519d4f8a85f197c0ef87a3d92234
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-12 11:42:53 +0900

    リファクタリング

--- EDCB/package.bat
+++ EDCB/package.bat
@@ -46,13 +46,15 @@ mkdir  "%dst_dir%\HttpPublic"
 mkdir  "%dst_dir%\PostBatExamples"
 
 
-COPY /V /B  "%src_dir%\EpgTimer.exe"        "%dst_dir%\"  /B
-COPY /V /B  "%src_dir%\EpgTimer.exe"        "%dst_dir%\EpgTimerNW.exe"  /B
-COPY /V /B  "%src_dir%\EpgTimerAdminProxy.exe"  "%dst_dir%\"  /B
-COPY /V /B  "%src_dir%\EpgTimerPlugIn.tvtp" "%dst_dir%\"  /B
-COPY /V /B  "%src_dir%\EpgTimerSrv.exe"     "%dst_dir%\"  /B
-COPY /V /B  "%src_dir%\EpgDataCap3.dll"     "%dst_dir%\"  /B
-COPY /V /B  "%src_dir%\SendTSTCP.dll"       "%dst_dir%\"  /B
+pushd  "%src_dir%\"
+COPY /V /B  "EpgTimer.exe"                  "%dst_dir%\"  /B
+COPY /V /B  "EpgTimer.exe"                  "%dst_dir%\EpgTimerNW.exe"  /B
+COPY /V /B  "EpgTimerAdminProxy.exe"        "%dst_dir%\"  /B
+COPY /V /B  "EpgTimerPlugIn.tvtp"           "%dst_dir%\"  /B
+COPY /V /B  "EpgTimerSrv.exe"               "%dst_dir%\"  /B
+COPY /V /B  "EpgDataCap3.dll"               "%dst_dir%\"  /B
+COPY /V /B  "SendTSTCP.dll"                 "%dst_dir%\"  /B
+popd
 
 pushd  "ini\Tools\IBonCast\%src2_dir%"
 COPY /V /B  "IBonCast.dll"                  "%dst_dir%\"  /B
@@ -62,18 +64,18 @@ COPY /V /B  "LICENSE-Civetweb.md"           "%dst_dir%\"  /B
 
 COPY /V /B  "Document\*.txt"                "%dst_dir%\"  /B
 
-COPY /V /B  "ini\Bitrate.ini"               "%dst_dir%\" /B
-COPY /V /B  "ini\BonCtrl.ini"               "%dst_dir%\" /B
-COPY /V /B  "ini\ContentTypeText.txt"       "%dst_dir%\" /B
-COPY /V /B  "ini\EpgTimer.exe.rd.xaml"      "%dst_dir%\" /B
-COPY /V /B  "ini\EpgTimerSrv_Install.bat"   "%dst_dir%\" /B
-COPY /V /B  "ini\EpgTimerSrv_Remove.bat"    "%dst_dir%\" /B
-COPY /V /B  "ini\EpgTimerSrv_Setting.bat"   "%dst_dir%\" /B
+COPY /V /B  "ini\Bitrate.ini"               "%dst_dir%\"  /B
+COPY /V /B  "ini\BonCtrl.ini"               "%dst_dir%\"  /B
+COPY /V /B  "ini\ContentTypeText.txt"       "%dst_dir%\"  /B
+COPY /V /B  "ini\EpgTimer.exe.rd.xaml"      "%dst_dir%\"  /B
+COPY /V /B  "ini\EpgTimerSrv_Install.bat"   "%dst_dir%\"  /B
+COPY /V /B  "ini\EpgTimerSrv_Remove.bat"    "%dst_dir%\"  /B
+COPY /V /B  "ini\EpgTimerSrv_Setting.bat"   "%dst_dir%\"  /B
 
 pushd  "EdcbPlugIn\EdcbPlugIn\"
-COPY /V /B  "ch2chset.vbs"                  "%dst_dir%\EdcbPlugIn\" /B
-COPY /V /B  "EdcbPlugIn.ini"                "%dst_dir%\EdcbPlugIn\" /B
-COPY /V /B  "EdcbPlugIn_Readme.txt"         "%dst_dir%\EdcbPlugIn\" /B
+COPY /V /B  "ch2chset.vbs"                  "%dst_dir%\EdcbPlugIn\"  /B
+COPY /V /B  "EdcbPlugIn.ini"                "%dst_dir%\EdcbPlugIn\"  /B
+COPY /V /B  "EdcbPlugIn_Readme.txt"         "%dst_dir%\EdcbPlugIn\"  /B
 popd
 
 XCOPY /E /V "ini\HttpPublic"                "%dst_dir%\HttpPublic\"
@@ -88,7 +90,7 @@ COPY /V /B  "watchip.ps1"                   "%dst_dir%\Tools\"  /B
 popd
 
 pushd  "%src_dir%"
-COPY /V /B  "SendTSTCP.dll"                 "%dst_dir%\" /B
+COPY /V /B  "SendTSTCP.dll"                 "%dst_dir%\"  /B
 COPY /V /B  "EdcbPlugIn.tvtp"               "%dst_dir%\EdcbPlugIn\"  /B
 COPY /V /B  "Write\Write_OneService.dll"    "%dst_dir%\EdcbPlugIn\"  /B
 COPY /V /B  "RecName\RecName_Macro.dll"     "%dst_dir%\RecName\"  /B
