08aa1016eeb7bdbb4a7118262e78ba14488a38cd
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-12 10:27:33 +0900

    リファクタリング

--- TVTest/common-build.bat
+++ TVTest/common-build.bat
@@ -1,6 +1,16 @@
 
-@ECHO ON
 setlocal
+
+
+@REM  ====================================================================
+@REM
+@REM   "全てのソリューションをビルドする"
+@REM
+
+set script_file=%0
+set script_dir=%~dp0
+pushd "%script_dir%"
+
 
 IF "%~1" == "" (
     echo no arguments passed.
@@ -13,13 +23,11 @@ set build_cmd=msbuild.exe  %common_args%
 
 
 @REM  ====================================================================
-@REM   "全てのソリューションをビルドする"
+@REM
+@REM   "ソリューションの再ターゲット"
 @REM
 
-set script_dir=%~dp0
-set prop_file="%script_dir%override.props"
-
-pushd "%script_dir%"
+set prop_file="%script_dir%..\override.props"
 dir "%prop_file%"
 
 set retarget_solution=-p:ForceImportBeforeCppProps=%prop_file%
@@ -28,8 +36,6 @@ set retarget_solution=-p:ForceImportBeforeCppProps=%prop_file%
 @REM  ----------------------------------------------------------------
 @REM   "TVTest  の全てのソリューションをビルドする"
 @REM
-
-pushd TVTest
 
 @REM   "LibISDB のビルド"
 
@@ -125,170 +131,23 @@ popd
 @REM   "TVTest  の全ソリューションのビルド完了"
 
 echo  TVTest  のビルド完了
-popd
+
 @REM  PAUSE
-
-@REM  ----------------------------------------------------------------
-@REM   "EDCB  の全てのソリューションをビルドする"
-@REM
-
-pushd EDCB
-
-@REM   "EDCB  のビルド"
-
-pushd  EDCB\Document
-%build_cmd%  -p:Platform=Win32 -p:Configuration=Debug       EDCB_ALL.VS2015.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=Win32 -p:Configuration=Release     EDCB_ALL.VS2015.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       EDCB_ALL.VS2015.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     EDCB_ALL.VS2015.sln
-IF errorlevel 1  GOTO  failure
-popd
-
-@REM   "EDCB  ツールのビルド"
-
-pushd  EDCB\ini\Tools
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       misc.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     misc.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       misc.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     misc.sln
-IF errorlevel 1  GOTO  failure
-popd
-
-@REM   "EDCB  ツールのビルド"
-
-pushd  EDCB\ini\Tools\IBonCast
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       IBonCast.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     IBonCast.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       IBonCast.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     IBonCast.sln
-IF errorlevel 1  GOTO  failure
-popd
-
-@REM   "EDCB  ツールのビルド"
-
-pushd  EDCB\ini\Tools\tsidmove
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       tsidmove.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     tsidmove.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       tsidmove.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     tsidmove.sln
-IF errorlevel 1  GOTO  failure
-popd
-
-@REM   "B25Decoder  のビルド"
-
-pushd  libaribb25
-%build_cmd%  -p:Platform=Win32 -p:Configuration=Debug       arib_std_b25.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=Win32 -p:Configuration=Release     arib_std_b25.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       arib_std_b25.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     arib_std_b25.sln
-IF errorlevel 1  GOTO  failure
-popd
-
-@REM   "lua52 のビルド"
-
-pushd  lua
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       lua52.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     lua52.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       lua52.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     lua52.sln
-IF errorlevel 1  GOTO  failure
-popd
-
-@REM   "zlib52  のビルド"
-
-pushd  lua-zlib
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       zlib52.sln
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     zlib52.sln
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       zlib52.sln
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     zlib52.sln
-popd
-
-@REM   "Write_Multi のビルド"
-
-pushd  Write_Multi
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       ^
-    %retarget_solution%  Write_Multi.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     ^
-    %retarget_solution%  Write_Multi.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       ^
-    %retarget_solution%  Write_Multi.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     ^
-    %retarget_solution%  Write_Multi.sln
-IF errorlevel 1  GOTO  failure
-popd
-
-@REM   "psisiarc  のビルド"
-
-pushd  psisiarc
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       psisiarc.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     psisiarc.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       psisiarc.sln
-IF errorlevel 1  GOTO  failure
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     psisiarc.sln
-IF errorlevel 1  GOTO  failure
-popd
-
-@REM   "psisimux  のビルド"
-
-pushd  psisimux
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       psisimux.sln
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     psisimux.sln
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       psisimux.sln
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     psisimux.sln
-popd
-
-@REM   "tsmemseg  のビルド"
-
-pushd  tsmemseg
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       tsmemseg.sln
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     tsmemseg.sln
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       tsmemseg.sln
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     tsmemseg.sln
-popd
-
-@REM   "tsreadex  のビルド"
-
-pushd  tsreadex
-%build_cmd%  -p:Platform=x64   -p:Configuration=Debug       tsreadex.sln
-%build_cmd%  -p:Platform=x64   -p:Configuration=Release     tsreadex.sln
-%build_cmd%  -p:Platform=x86   -p:Configuration=Debug       tsreadex.sln
-%build_cmd%  -p:Platform=x86   -p:Configuration=Release     tsreadex.sln
-popd
-
-
-@REM   "EDCB  の全ソリューションのビルド完了"
-
-echo  EDCB  のビルド完了
-popd
-REM   PAUSE
-
-GOTO  success
 
 
 @REM  ====================================================================
+@REM
+@REM   "完了"
+@REM
+
+popd
+echo  全ソリューションのビルド完了
+
+EXIT  /B  0
+
+
+@REM  ====================================================================
+@REM
 @REM   "ビルド失敗"
 @REM
 
@@ -308,14 +167,5 @@ echo  ビルドに失敗しました : %build_error%
 IF  %build_error% LSS 1 (
     set build_error=1
 )
+
 EXIT /B %build_error%
-
-
-@REM  ====================================================================
-@REM   "全てのソリューションのビルド完了"
-@REM
-
-:success
-echo  全ソリューションのビルド完了
-popd
-PAUSE
